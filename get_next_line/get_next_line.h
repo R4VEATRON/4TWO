4e8a92540c82042fadae98540f3d69bc2732f037
R4VEATRON <abdulrahman.elsafi1135@gmail.com>
2025-12-16 17:09:21 +0400

    Add files via upload

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelsafi <aelsafi@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/16 16:36:01 by aelsafi           #+#    #+#             */
+/*   Updated: 2025/12/16 16:36:03 by aelsafi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE 
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strdup(const char *str);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_substr(const char *str, unsigned int start, size_t len);
+
+#endif
