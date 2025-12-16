098f00ba8086d959b929491163de0a18c3ed37a6
R4VEATRON <abdulrahman.elsafi1135@gmail.com>
2025-12-16 13:16:35 +0400

    Add files via upload

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelsafi <aelsafi@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/16 11:47:19 by aelsafi           #+#    #+#             */
+/*   Updated: 2025/12/16 11:47:19 by aelsafi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char *free_all(char *buffer, char *unread_data)
+{
+    free (buffer);
+    free (unread_data);
+    return (NULL);
+}
+
+static char *read_until_newline(int fd, char *unread_data)
+{
+    char    *buffer;
+    int     bytes_read;
+    char    *temp;
+
+    buffer = (char *)malloc(BUFFER_SIZE + 1);
+    if (!buffer)
+        return (NULL);
+    bytes_read = 1;
+    while (!ft_strchr(unread_data, '\n') && bytes_read > 0)
+    {
+        bytes_read = read(fd, buffer, BUFFER_SIZE);
+        if (bytes_read < 0)
+            return (free_all(buffer, unread_data));
+        buffer[bytes_read] = '\0';
+        temp = ft_strjoin(unread_data, buffer);
+        if (!temp)
+            return (free_all(buffer, unread_data));
+        free(unread_data);
+        unread_data = temp;
+    }
+    free (buffer);
+    buffer = NULL;
+    return (unread_data);
+}
+
+static char *extract_line(char *unread_data)
+{
+    size_t  line_len;
+    char    *line;
+
+    line_len = 0;
+    if (!unread_data || *unread_data == '\0')
+        return (NULL);
+    while (unread_data[line_len] && unread_data[line_len] != '\n')
+        line_len++;
+    line = ft_substr(unread_data, 0, line_len);
+    return (line);
+}
+
+static char*update_unread_data(char *unread_data)
+{
+    size_t  i;
+    char    *new_data;
+
+    if (!unread_data || *unread_data == '\0')
+    {
+        free(unread_data);
+        return (NULL);
+    }
+    i = 0;
+    while (unread_data[i] && unread_data[i] != '\n')
+        i++;
+    new_data = ft_substr(unread_data, i + 1, ft_strlen(unread_data) - i -1);
+    free(unread_data);
+    if (!new_data || *new_data == '\0')
+    {
+        free(new_data);
+        new_data = NULL;
+    }
+    return (new_data);
+}
+
+char    *get_next_line(int fd)
+{
+    static char *unread_data;
+    char        *line;
+
+    if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE >= INT_MAX)
+        return (NULL);
+    unread_data = read_until_newline(fd, unread_data);
+    if (!unread_data || *unread_data == '\0')
+    {
+        free(unread_data);
+        unread_data = NULL;
+        return (NULL);
+    }
+    line = extract_line(unread_data);
+    if (line == NULL)
+    {
+        free(unread_data);
+        unread_data = NULL;
+    }
+    else
+        unread_data = update_unread_data(unread_data);
+    return (line);
+}
---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,116 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelsafi <aelsafi@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/16 13:11:19 by aelsafi           #+#    #+#             */
+/*   Updated: 2025/12/16 13:11:19 by aelsafi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	if (!str)
+		return (NULL);
+	while (*str)
+	{
+		if (*str == (char)c)
+			return ((char *)str);
+		str++;
+	}
+	if ((char)c == '\0')
+		return ((char *)str);
+	return (NULL);
+}
+
+char	*ft_strdup(const char *str)
+{
+	size_t	len;
+	size_t	i;
+	char	*dup;
+
+	if (str == NULL)
+		return (NULL);
+	len = ft_strlen(str);
+	dup = (char *)malloc(sizeof(char) * (len + 1));
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		dup[i] = str[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*join;
+
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
+	join = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!join)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		join[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+		join[i++] = s2[j++];
+	join[i] = '\0';
+	return (join);
+}
+
+char	*ft_substr(const char *str, unsigned int start, size_t len)
+{
+	size_t	s_len;
+	size_t	i;
+	char	*substr;
+
+	if (!str)
+		return (NULL);
+	s_len = ft_strlen(str);
+	if (len == 0 || start >= s_len)
+		return (ft_strdup(""));
+	if (len > s_len - start)
+		len = s_len - start;
+	substr = (char *)malloc(sizeof(char) * (len + 1));
+	if (!substr)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		substr[i] = str[start + i];
+		i++;
+	}
+	substr[i] = '\0';
+	return (substr);
+}
