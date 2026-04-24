fee4fdeff707a37ef29b75e2305caf9e76564f19
R4VEATRON <abdulrahman.elsafi1135@gmail.com>
2026-04-24 13:49:57 +0000

    python00 complete

---
+++ piscine_python/ex6/ft_count_harvest_recursive.py
@@ -0,0 +1,11 @@
+def ft_count_harvest_recursive():
+    days = int(input("Days until harvest: "))
+
+    def count(day, limit):
+        if day > limit:
+            print("Harvest time!")
+            return
+        print(f"Day {day}")
+        count(day + 1, limit)
+
+    count(1, days)
